767f727297af0b2d940307f051eca730cc387bea
gedjien <113451975+gedjien@users.noreply.github.com>
2024-12-10 12:39:36 +0300

    Delete SlideShowIterator directory

--- SlideShowIterator/src/main/java/com/example/slideshowiterator/Aggregate.java
+++
@@ -1,5 +0,0 @@
-package com.example.slideshowiterator;
-
-public interface Aggregate {
-    Iterator getIterator();
-}
--- SlideShowIterator/src/main/java/com/example/slideshowiterator/Iterator.java
+++
@@ -1,6 +0,0 @@
-package com.example.slideshowiterator;
-
-public interface Iterator {
-    boolean hasNext();
-    Object next();
-}
